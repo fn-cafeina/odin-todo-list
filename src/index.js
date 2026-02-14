fa6e1af6378fb8826c0d9e676615ed2f2ef19e3f
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-14 13:21:38 -0600

    Add open and close dialog events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import "./styles.css";
+
+const dialog = document.querySelector("dialog");
+const createTaskBtn = document.querySelector(".create-task-btn");
+const closeDialogBtn = document.querySelector(".close-dialog-btn");
+
+createTaskBtn.addEventListener("click", () => dialog.showModal());
+closeDialogBtn.addEventListener("click", () => dialog.close());
