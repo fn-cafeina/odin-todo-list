b96833f47d7d56ab08a597b52ae1f5a640af8653
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-14 13:33:29 -0600

    Add allday option in create task dialog

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import "./styles.css";
 const dialog = document.querySelector("dialog");
 const createTaskBtn = document.querySelector(".create-task-btn");
 const closeDialogBtn = document.querySelector(".close-dialog-btn");
-
 createTaskBtn.addEventListener("click", () => dialog.showModal());
 closeDialogBtn.addEventListener("click", () => dialog.close());
+
+const taskAlldayOption = document.querySelector("#task-allday-option");
+const taskDuedate = document.querySelector("#task-duedate");
+taskAlldayOption.addEventListener("change", () => {
+  if (taskAlldayOption.checked) {
+    taskDuedate.setAttribute("type", "date");
+  } else {
+    taskDuedate.setAttribute("type", "datetime-local");
+  }
+});
