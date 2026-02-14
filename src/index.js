cce92dcabf2ce110d50a3714c5af6c6f83fd9e1f
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-14 17:16:48 -0600

    Min changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import "./styles.css";
 const dialog = document.querySelector("dialog");
 const createTaskBtn = document.querySelector(".create-task-btn");
 const closeDialogBtn = document.querySelector(".close-dialog-btn");
-createTaskBtn.addEventListener("click", () => dialog.showModal());
-closeDialogBtn.addEventListener("click", () => dialog.close());
+
+createTaskBtn.onclick = () => dialog.showModal();
+closeDialogBtn.onclick = () => dialog.close();
 
 const taskAlldayOption = document.querySelector("#task-allday-option");
 const taskDuedate = document.querySelector("#task-duedate");
